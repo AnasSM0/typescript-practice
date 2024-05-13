fe78700d72d37115b417b9d73753584a9834aee4
AnasSM0 <anasbutt20067@gmail.com>
2024-05-13 16:45:38 +0500

    first commit

---
+++ variables/main.js
@@ -0,0 +1,9 @@
+var pi = 3.14;
+var check;
+if (pi != 3.14) {
+    check = false;
+}
+else {
+    check = true;
+}
+console.log(check);
---
+++ variables/main.ts
@@ -0,0 +1,12 @@
+const pi = 3.14; 
+let check:boolean;
+
+if (pi!=3.14){
+
+check = false;
+
+}
+
+else{check=true;}
+
+console.log(check);
