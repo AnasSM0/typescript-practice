e56c06375b2c6e43b231f71748507f439a609a85
AnasSM0 <anasbutt20067@gmail.com>
2024-05-20 10:59:59 +0500

    second commit

--- objects/src/main.ts
+++ objects/src/main.ts
@@ -28,7 +28,7 @@ console.log(car.oilChange());
 
 console.log("---------------------------------")
 */
-interface Car {
+interface vehicle {
     make: string;
     model: string;
     year: number;
@@ -40,7 +40,7 @@ interface Car {
     oilChange?: () => string; // Optional method
   }
   
-  const car1: Car = {
+  const car1: vehicle = {
     make: "Honda",
     model: "NSX",
     year: 1996,
@@ -67,9 +67,9 @@ interface Car {
     console.log(car1.oilChange());
   }
 
-  console.log("------------------------------");
+  console.log("--------------------------------------------");
 
-  const car2: Car = {
+  const car2: vehicle = {
     make: "Mercedes",
     model: "C63",
     year: 2008,
@@ -98,3 +98,45 @@ interface Car {
   if (car2.oilChange) {
     console.log(car2.oilChange());
   }
+
+  interface plane extends vehicle{
+
+    ammunation:boolean;
+    pilot:string;
+
+    fireMissiles: () => string;
+
+  }
+
+  const jet: plane = {
+
+    make:"Lockheed Martin",
+    model: "F16 Raptor",
+    year:2008,
+    available:true,
+    colors:["gray","black","silver"],
+    ammunition: true,
+    pilot: "Captain Maverick",
+
+    getinfo: function () {
+        return `${this.make} ${this.model}`;
+      },
+    
+      displayinfo: function () {
+        console.log(`This is a ${this.getinfo()} built in ${this.year}`);
+      } , 
+      
+      engineSound: () => 'WHOOOSH',
+
+      fireMissiles: function () {
+        return(`${this.make} ${this.model} is firing missiles!`);
+      },
+    
+      
+    };
+
+    console.log("--------------------------------------------");
+
+  jet.displayinfo();
+  console.log("It goes",jet.engineSound());
+  console.log(jet.fireMissiles());
