f3518f86be202d55698cb2af33a4e5fc00a75d42
AnasSM0 <anasbutt20067@gmail.com>
2024-05-20 10:36:40 +0500

    second commit

--- objects/src/main.ts
+++ objects/src/main.ts
@@ -1,25 +1,100 @@
-const car={
+/*const car = {
+  make: "Honda",
+  model: "NSX",
+  year: 1996,
+  available: true,
+  colors: ["red", "blue", "black", "white"],
 
-make: "Honda",
-model: "NSX",
-year: 1996,
-available: true,
-colors: ["red","blue","black","white"],
+  getinfo: function () {
+    return `${this.make} ${this.model}`;
+  },
 
-getinfo:function(){
+  displayinfo: function () {
+    console.log(`This is a ${this.getinfo()} built in ${this.year}`);
+  },
 
-return `${this.make} ${this.model}`
+  engineSound:()=> 'vroom vroom',
 
-},
+  oilChange:function(){
 
- displayinfo:function(){
+    return `${this.make} ${this.model} requires an oil change.`
+  }
 
-console.log(`This is a ${this.getinfo()} built in ${this.year}`)
-        
-}
-
-}
+};
 
 car.displayinfo();
+console.log("It goes", car.engineSound());
+console.log(car.oilChange());
 
+console.log("---------------------------------")
+*/
+interface Car {
+    make: string;
+    model: string;
+    year: number;
+    available: boolean;
+    colors: string[];
+    getinfo: () => string;
+    displayinfo: () => void;
+    engineSound: () => string;
+    oilChange?: () => string; // Optional method
+  }
+  
+  const car1: Car = {
+    make: "Honda",
+    model: "NSX",
+    year: 1996,
+    available: true,
+    colors: ["red", "blue", "black", "white"],
+  
+    getinfo: function () {
+      return `${this.make} ${this.model}`;
+    },
+  
+    displayinfo: function () {
+      console.log(`This is a ${this.getinfo()} built in ${this.year}`);
+    },
+  
+    engineSound: () => 'vroom vroom',
+  
+  };
+  
+  car1.displayinfo();
+  console.log("It goes", car1.engineSound());
+  
+  // Check if the optional method exists before calling it
+  if (car1.oilChange) {
+    console.log(car1.oilChange());
+  }
 
+  console.log("------------------------------");
+
+  const car2: Car = {
+    make: "Mercedes",
+    model: "C63",
+    year: 2008,
+    available: true,
+    colors: ["red", "yellow", "black", "gray"],
+  
+    getinfo: function () {
+      return `${this.make} ${this.model}`;
+    },
+  
+    displayinfo: function () {
+      console.log(`This is a ${this.getinfo()} built in ${this.year}`);
+    },
+  
+    engineSound: () => 'VRROOOOM VROOOOM',
+  
+    oilChange: function () {
+      return `${this.make} ${this.model} requires an oil change.`;
+    }
+  };
+  
+  car2.displayinfo();
+  console.log("It goes", car2.engineSound());
+  
+  // Check if the optional method exists before calling it
+  if (car2.oilChange) {
+    console.log(car2.oilChange());
+  }
