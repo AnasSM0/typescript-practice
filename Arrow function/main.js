c9cbbb9b8bb15f6fc66b06c608f9b8394b54825b
AnasSM0 <anasbutt20067@gmail.com>
2024-06-10 11:54:12 +0500

    first commit

---
+++ Arrow function/main.js
@@ -0,0 +1,5 @@
+var add = function (a, b) {
+    var sum = a + b;
+    return sum;
+};
+console.log(add(5, 6));
---
+++ Arrow function/main.ts
@@ -0,0 +1,8 @@
+const add = (a: number, b: number ): number => { 
+
+let sum = a + b;
+return sum;
+
+};
+
+console.log(add (5,6));
